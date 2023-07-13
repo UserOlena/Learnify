7c0aff384523793c2faa9740cf77a48e8545ebbf
UserOlena <helen.watermore@gmail.com>
2023-07-13 02:30:42 -0700

    Include User typeDefs and query by user ID.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,17 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    password: String
+  }
+
+  type Query {
+    user(_id: ID!): User!
+  }
+
+`;
+
+module.exports = typeDefs;
