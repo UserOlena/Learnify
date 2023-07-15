e4c52a6f34c2f5ff353ace3bc4fed2d709eadd3e
rebeccaroos <rebecca_roos@berkeley.edu>
2023-07-14 19:40:00 -0700

    Add configuration object to Tutorial.js

--- server/models/Tutorial.js
+++ server/models/Tutorial.js
@@ -1,45 +1,52 @@
 const { Schema, model } = require('mongoose');
 
-const tutorialSchema = new Schema({
-    title: {
-        type: String,
-        trim: true,
-        required: [true, 'Title is required!'],
+const tutorialSchema = new Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            required: [true, 'Title is required!'],
+        },
+        overview: {
+            type: String,
+            trim: true,
+            required: [true, 'Overview is required!'],
+        },
+        thumbnail: {
+            type: String,
+            required: [true, 'Thumbnail is required!'],
+        },
+        categories: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Category',
+            },
+        ],
+        teacherId: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+        lessons: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Lesson',
+            },
+        ],
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Review',
+            },
+        ],
     },
-    overview: {
-        type: String,
-        trim: true,
-        required: [true, 'Overview is required!'],
-    },
-    thumbnail: {
-        type: String,
-        required: [true, 'Thumbnail is required!'],
-    },
-    categories: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Category',
-        },
-    ],
-    teacherId: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-        },
-    ],
-    lessons: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Lesson',
-        },
-    ],
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review',
-        },
-    ],
-});
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
 
 // Calculate the total duration of the tutorial by adding the durations of the individual lessons
 tutorialSchema.virtual('totalDuration').get(function() {
