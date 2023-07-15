f8249b9736717595a1d1f3ad6535479e88586346
rebeccaroos <rebecca_roos@berkeley.edu>
2023-07-15 13:32:11 -0700

    Add seeds for all schemas

---
+++ server/seeds/reviewSeeds.js
@@ -0,0 +1,84 @@
+const reviewSeeds = [
+    {
+        rating: 3,
+        comment: 'This tutorial provided clear and concise instructions that were easy to follow.'
+    },
+    {
+        rating: 4,
+        comment: 'I found the tutorial to be incredibly informative and helpful in my learning journey.'
+    },
+    {
+        rating: 3,
+        comment: 'The tutorial exceeded my expectations with its practical insights and actionable advice.'
+    },
+    {
+        rating: 5,
+        comment: 'I appreciate how the tutorial broke down complex concepts into understandable chunks.'
+    },
+    {
+        rating: 4,
+        comment: 'The tutorial\'s step-by-step approach made it accessible and enjoyable for beginners.'
+    },
+    {
+        rating: 3,
+        comment: 'The tutorial offered valuable tips and techniques that I can immediately apply in real-life situations.'
+    },
+    {
+        rating: 4,
+        comment: 'I was impressed by the tutorial\'s depth of coverage on the subject matter.'
+    },
+    {
+        rating: 4,
+        comment: 'The tutorial\'s examples and illustrations greatly enhanced my understanding of the topic.'
+    },
+    {
+        rating: 5,
+        comment: 'I feel more confident and knowledgeable after completing the tutorial.'
+    },
+    {
+        rating: 3,
+        comment: 'The tutorial provided a solid foundation for further exploration and learning.'
+    },
+    {
+        rating: 4,
+        comment: 'I appreciated the tutorial\'s practical exercises that reinforced the learning material.'
+    },
+    {
+        rating: 3,
+        comment: 'The tutorial\'s pacing was perfect, allowing me to grasp concepts at my own pace.'
+    },
+    {
+        rating: 3,
+        comment: 'The tutorial\'s organization and structure made it easy to navigate and locate specific information.'
+    },
+    {
+        rating: 4,
+        comment: 'I found the tutorial to be engaging and kept me interested throughout the entire learning process.'
+    },
+    {
+        rating: 5,
+        comment: 'The tutorial\'s explanations were concise yet comprehensive, making complex ideas easier to grasp.'
+    },
+    {
+        rating: 4,
+        comment: 'The tutorial\'s real-life examples helped me see the practical applications of the concepts taught.'
+    },
+    {
+        rating: 3,
+        comment: 'I particularly enjoyed the tutorial\'s interactive elements that enhanced the learning experience.'
+    },
+    {
+        rating: 2,
+        comment: 'The tutorial\'s resources and recommended further readings were valuable additions to the learning material.'
+    },
+    {
+        rating: 4,
+        comment: 'The tutorial\'s supportive community and discussion forums created a collaborative learning environment.'
+    },
+    {
+        rating: 5,
+        comment: 'I would highly recommend this tutorial to anyone looking to expand their knowledge in the subject.'
+    },
+];
+
+module.exports = reviewSeeds;
