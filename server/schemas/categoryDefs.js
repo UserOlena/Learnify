3242428865092f52150a21c7e18d2b9ebd316e14
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-07-16 22:45:54 -0700

    add mutations for ADD/UPDATE/DELETE categories that also update associated tutorials

--- server/schemas/categoryDefs.js
+++ server/schemas/categoryDefs.js
@@ -1,8 +1,7 @@
 const { gql } = require('apollo-server-express');
 
-const { Category } = require('../models');
+const { Category, Tutorial } = require('../models');
 
-// TODO: Complete Category typeDefs
 const categoryTypeDefs = gql`
   type Category {
     _id: ID!
@@ -11,14 +10,97 @@ const categoryTypeDefs = gql`
 
   type Query {
     categories: [Category]
+    category(_id: ID!): Category
+  }
+
+  type Mutation {
+    addCategory(tutorialId: ID!, category: String!): Category
+    updateCategory(_id: ID!, category: String!): Category
+    deleteCategory(_id: ID!): Category
   }
 `;
 
-// TODO: Complete Resolvers for Category typeDefs
+
 const categoryResolvers = {
   Query: {
+    //GET all categories
     categories: async () => {
       return await Category.find({});
+    },
+
+    //GET a single category by ID
+    category: async (parent, {_id }) => {
+      return await Category.findById(_id);
+    }
+  },
+  Mutation: {
+    // ADD a category and attach it to a tutorial
+    addCategory: async (parent, { tutorialId, category }) => {
+      const newCategory = await Category.create({ category });
+
+      await Tutorial.findByIdAndUpdate(
+        tutorialId,
+        {$push: { categories: newCategory._id } },
+        { new: true }
+      );
+      return newCategory;
+    },
+    //UPDATE a category and its associated tutuorials
+    updateCategory: async (parent, { _id, category }) => {
+      try {
+        // Find the category by ID
+        const updatedCategory = await Category.findByIdAndUpdate(
+          _id, 
+          { category }, 
+          { new: true }
+        );
+    
+        if (!updatedCategory) {
+          throw new Error('Category not found');
+        }
+    
+        // Find tutorials associated with updated category
+        const tutorials = await Tutorial.find({ categories: updatedCategory._id });
+    
+        // Update the category in each associated tutorial
+        await Promise.all(
+          tutorials.map((tutorial) => {
+            tutorial.categories.forEach((categoryId, index) => {
+              if (categoryId.equals(updatedCategory._id)) {
+                tutorial.categories[index] = updatedCategory._id;
+              }
+            });
+            return tutorial.save();
+          })
+        );
+    
+        return updatedCategory;
+      } catch (error) {
+        throw new Error(`Failed to update category: ${error.message}`);
+      }
+    },
+
+    //DELETE a category and remove it from any associated tutorials
+    deleteCategory: async (parent, {_id}) => {
+      const categoryToDelete = await Category.findById(_id);
+
+      if (!categoryToDelete) {
+        throw new Error('Category not found');
+      }
+
+      //find tutorials associated with the category
+      const tutorials = await Tutorial.find({ categories: 
+      categoryToDelete._id });
+
+      //remove the category from each associated tutorial
+      await Promise.all(
+        tutorials.map((tutorial) => {
+          tutorial.categories.pull(categoryToDelete._id);
+          return tutorial.save();
+        })
+      )
+      return await Category.findByIdAndDelete(_id);
+
     }
   }
 };
