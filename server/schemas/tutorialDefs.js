967b3ea1b45c512bc76a52c8a5f3fd3fe33f2395
rebeccaroos <rebecca_roos@berkeley.edu>
2023-07-16 18:28:58 -0700

    Add typeDefs and resolvers for tutorials

--- server/schemas/tutorialDefs.js
+++ server/schemas/tutorialDefs.js
@@ -2,7 +2,6 @@ const { gql } = require('apollo-server-express');
 
 const { Tutorial } = require('../models');
 
-// TODO: Complete Tutorial typeDefs
 const tutorialTypeDefs = gql`
   type Tutorial {
     _id: ID!
@@ -10,25 +9,77 @@ const tutorialTypeDefs = gql`
     overview: String
     thumbnail: String
     categories: [Category]
-    teacherId: [User]
+    teacher: [User]
     lessons: [Lesson]
     reviews: [Review]
+    totalDuration: Int
   }
 
   type Query {
     tutorials: [Tutorial]
+    tutorial(_id: ID!): Tutorial
+  }
+
+  type Mutation {
+    addTutorial(title: String!, overview: String!, thumbnail: String!, categories: [ID]!, teacher: [ID]!): Tutorial
+    updateTutorial(_id: ID!, title: String, overview: String, thumbnail: String, categories: [ID]): Tutorial
+    deleteTutorial(_id: ID!): Tutorial
   }
 `;
 
-// TODO: Complete Resolvers for Tutorial typeDefs
 const tutorialResolvers = {
   Query: {
+    // Get all tutorials
     tutorials: async () => {
       return await Tutorial.find({})
         .populate('categories')
-        .populate('teacherId')
+        .populate('teacher')
         .populate('lessons')
         .populate('reviews');
+    },
+
+    // Get a single tutorial by ID
+    tutorial: async (parent, { _id }) => {
+      return await Tutorial.findById(_id)
+        .populate('categories')
+        .populate('teacher')
+        .populate('lessons')
+        .populate('reviews');
+    },
+  },
+  Mutation: {
+    // Add a tutorial
+    addTutorial: async (parent, { title, overview, thumbnail, categories, teacher }) => {
+      return await Tutorial.create({ title, overview, thumbnail, categories, teacher });
+    },
+
+    // Update a tutorial's title, overview, thumbnail, and/or categories
+    updateTutorial: async (parent, { _id, title, overview, thumbnail, categories }) => {
+      // Create an updates object only containing the updated fields
+      const updates = {};
+      if (title) {
+        updates.title = title;
+      }
+      if (overview) {
+        updates.overview = overview;
+      }
+      if (thumbnail) {
+        updates.thumbnail = thumbnail;
+      }
+      if (categories) {
+        updates.categories = categories;
+      }
+
+      return await Tutorial.findByIdAndUpdate(
+        _id,
+        { $set: updates },
+        { new: true },
+      );
+    },
+
+    // Delete a tutorial
+    deleteTutorial: async (parent, { _id }) => {
+      return await Tutorial.findByIdAndDelete(_id);
     }
   }
 };
