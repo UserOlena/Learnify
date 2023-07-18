d03482c58cf094fc7cbea8b1674d6497a467b678
sendusyourbones <beckee.roos@gmail.com>
2023-07-18 15:12:01 -0700

    Add try/catch to all resolvers. Add .populate to tutorialsByCategory.

--- server/schemas/tutorialDefs.js
+++ server/schemas/tutorialDefs.js
@@ -31,27 +31,39 @@ const tutorialTypeDefs = gql`
 const tutorialResolvers = {
   Query: {
     // Get all tutorials
-    tutorials: async function() {
-      return await Tutorial.find({})
+    tutorials: async function () {
+      try {
+        return await Tutorial.find({})
         .populate('categories')
         .populate('teacher')
         .populate('lessons')
         .populate('reviews');
+      } catch (error) {
+        throw new Error(`Failed to fetch tutorials: ${error.message}`);
+      }
     },
 
     // Get a single tutorial by ID
-    tutorial: async function(parent, { _id }) {
-      return await Tutorial.findById(_id)
+    tutorial: async function (parent, { _id }) {
+      try {
+        return await Tutorial.findById(_id)
         .populate('categories')
         .populate('teacher')
         .populate('lessons')
         .populate('reviews');
+      } catch (error) {
+        throw new Error(`Failed to fetch single tutorial: ${error.message}`);
+      }
     },
 
     // Get all tutorials in a single category by category ID
     tutorialsByCategory: async function(parent, { categoryId }) {
       try {
-        return await Tutorial.find({ categories: categoryId });
+        return await Tutorial.find({ categories: categoryId })
+          .populate('categories')
+          .populate('teacher')
+          .populate('lessons')
+          .populate('reviews');
       } catch (error) {
         throw new Error(`Failed to fetch tutorials by category: ${error.message}`);
       }
@@ -60,37 +72,49 @@ const tutorialResolvers = {
 
   Mutation: {
     // Add a tutorial
-    addTutorial: async function(parent, { title, overview, thumbnail, categories, teacher }) {
-      return await Tutorial.create({ title, overview, thumbnail, categories, teacher });
+    addTutorial: async function (parent, { title, overview, thumbnail, categories, teacher }) {
+      try {
+        return await Tutorial.create({ title, overview, thumbnail, categories, teacher });
+      } catch (error) {
+        throw new Error(`Failed to add tutorial: ${error.message}`);
+      }
     },
 
     // Update a tutorial's title, overview, thumbnail, and/or categories
     updateTutorial: async function(parent, { _id, title, overview, thumbnail, categories }) {
-      // Create an updates object only containing the updated fields
-      const updates = {};
-      if (title) {
-        updates.title = title;
-      }
-      if (overview) {
-        updates.overview = overview;
-      }
-      if (thumbnail) {
-        updates.thumbnail = thumbnail;
-      }
-      if (categories) {
-        updates.categories = categories;
-      }
+      try {
+        // Create an updates object only containing the updated fields
+        const updates = {};
+        if (title) {
+          updates.title = title;
+        }
+        if (overview) {
+          updates.overview = overview;
+        }
+        if (thumbnail) {
+          updates.thumbnail = thumbnail;
+        }
+        if (categories) {
+          updates.categories = categories;
+        }
 
-      return await Tutorial.findByIdAndUpdate(
-        _id,
-        { $set: updates },
-        { new: true },
-      );
+        return await Tutorial.findByIdAndUpdate(
+          _id,
+          { $set: updates },
+          { new: true },
+        );
+      } catch (error) {
+        throw new Error(`Failed to update tutorial: ${error.message}`);
+      }
     },
 
     // Delete a tutorial
-    deleteTutorial: async function(parent, { _id }) {
-      return await Tutorial.findByIdAndDelete(_id);
+    deleteTutorial: async function (parent, { _id }) {
+      try {
+        return await Tutorial.findByIdAndDelete(_id);
+      } catch (error) {
+        throw new Error(`Failed to delete tutorial: ${error.message}`);
+      }
     },
   },
 };
