2145c6140164d573ad974455b4b9ef2b32151376
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-07-17 23:58:21 -0700

    add try/catch blocks to queries and mutations

--- server/schemas/lessonDefs.js
+++ server/schemas/lessonDefs.js
@@ -31,19 +31,27 @@ const lessonResolvers = {
   Query: {
     //get all lessons
     lessons: async function () {
+      try {
       return await Lesson.find({});
+      } catch (error) {
+        throw new Error(`Failed to retrieve lessons: ${error.message}`);
+      }
     },
     //get a single lesson by id
     lesson: 
         async function (parent, { _id }) {
+          try {
             return await Lesson.findById(_id);
+          } catch (error) {
+            throw new Error(`Failed to retrieve lesson: ${error.message}`);
+          }
         },
   },
 
   Mutation: {
     //ADD a new lesson and attach it to its tutorial
     addLesson: async function (parent, { tutorialId, name, body, media, duration }) {
-       
+       try{ 
       const newLesson = await Lesson.create({ name, body, media, duration });
 
       await Tutorial.findByIdAndUpdate(
@@ -53,11 +61,14 @@ const lessonResolvers = {
       );
 
       return newLesson;
-
+      } catch (error) {
+        throw new Error(`Failed to add lesson: ${error.message}`);
+      }
     },
     
     //UPDATE one or more fields for an existing lesson
     updateLesson: async function (parent, {_id, name, body, media, duration}) {
+      try {
       //create object containing only field(s) to be updated
       const updates = {};
       if (name) {
@@ -78,11 +89,18 @@ const lessonResolvers = {
         { $set: updates }, 
         {new: true }
         );
+      } catch (error) {
+        throw new Error(`Failed to update lesson: ${error.message}`);
+      }
     },
 
     //DELETE a lesson
     deleteLesson: async function (parent, {_id}) {
+      try {
       return await Lesson.findByIdAndDelete(_id);
+      } catch (error) {
+        throw new Error(`Failed to delete lesson: ${error.message}`);
+      }
     }
 }
 };
