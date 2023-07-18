126810006965101140820d8155c85174dd60d720
UserOlena <122197592+UserOlena@users.noreply.github.com>
2023-07-17 23:58:53 -0700

    userMutation.js_Change the names for User Mutation

--- client/src/utils/userMutation.js
+++ client/src/utils/userMutation.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const login = gql`
+export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
@@ -12,7 +12,7 @@ export const login = gql`
   }
 `;
 
-export const addUser = gql`
+export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
