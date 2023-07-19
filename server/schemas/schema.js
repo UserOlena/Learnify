b2820b7cef1b1768da4eeac95db742a8d0bf8c39
Riley O'Neil <37202991+rileydoneil@users.noreply.github.com>
2023-07-18 18:35:23 -0700

    Delete schema.js

--- server/schemas/schema.js
+++
@@ -1,9 +0,0 @@
-import { lessonTypes } from './typeDefs';
-import { lessonResolvers, userResolvers } from './resolvers';
-const { mergeStrings } = require('graphql-tools');
-
-const typeDefs = mergeStrings([lessonTypes]);
-
-const resolvers = mergeStrings([lessonResolvers, userResolvers])
-
-module.exports = { typeDefs, resolvers }
