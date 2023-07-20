505ebed06eed9551372e7afc692aa97317f13a3d
sendusyourbones <beckee.roos@gmail.com>
2023-07-20 15:33:51 -0700

    Update query tutorials file path

--- client/src/components/Recommended.js
+++ client/src/components/Recommended.js
@@ -4,7 +4,7 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import { Card, CardContent, Typography, IconButton } from '@material-ui/core';
 import { ArrowBack, ArrowForward } from '@material-ui/icons';
 import { useQuery } from '@apollo/client';
-import { QUERY_TUTORIALS } from '../utils/tutorialQuery';
+import { QUERY_TUTORIALS } from '../utils/queries/tutorialQueries';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
