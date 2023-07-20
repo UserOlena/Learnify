14390b75216cab4f0ccc48bcf3fef0fd47b066c5
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-07-19 21:46:30 -0700

    merge origin main with tutorial-page on local

--- client/src/pages/Tutorial.js
+++ client/src/pages/Tutorial.js
@@ -1,12 +1,40 @@
 import { React } from 'react';
 
-import { Box, Card, Container, Grid, makeStyles, Typography } from '@material-ui/core';
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import {
+  Info,
+  InfoCaption,
+  InfoSubtitle,
+  InfoTitle,
+} from '@mui-treasury/components/info';
+import { useCoverCardMediaStyles } from '@mui-treasury/styles/cardMedia/cover';
 
 import { Rating } from '@material-ui/lab';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
+  },
+  media: {},
+  banner: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    bottom: 0,
+    left: 0,
+    zIndex: 2,
+    backgroundColor: 'rgba(0, 0, 0, 0.08)',
+
   },
 });
 
@@ -16,30 +44,60 @@ const useStyles = makeStyles({
 //TODO: GET_RUNTIME (line 40)
 
 export function Tutorial() {
+  const mediaStyles = useCoverCardMediaStyles({ bgPosition: 'top' });
   const classes = useStyles();
 
   return (
     <div className='root'>
-    <Container maxWidth='sm'>
+      <Container maxWidth='sm'>
         <Typography
           variant='h2'
           style={{
             color: '#283845',
-            fontWeight: 'bold'
+            fontWeight: 'bold',
           }}
         >
           Tutorial Title
         </Typography>
         <Box>
-        <Rating name="read-only" value='{value}' readOnly />
-        <Typography variant='subtitle1'># Ratings</Typography>
-      </Box>
-      <Box>
-        <Typography variant='h4'>TeacherName</Typography>
-        <Typography variant='h5'>Runtime: "value" </Typography>
-      </Box>
-    </Container>
-    
+          <Rating
+            name='read-only'
+            value='{value}'
+            readOnly
+          />
+          <Typography variant='subtitle1'># Ratings</Typography>
+        </Box>
+        <Box>
+          <Typography variant='h4'>TeacherName</Typography>
+          <Typography variant='h5'>Runtime: "value" </Typography>
+        </Box>
+      </Container>
+      <Container>
+        <Grid 
+            container
+            className={classes.info}
+            direction='row'>
+          <Grid item>
+            <Box
+              position={'relative'}
+              width={'100%'}
+              height={'100%'}
+              p={2}
+            >
+              <CardMedia
+                classes={mediaStyles}
+                image={
+                  'https://docs.google.com/drawings/d/e/2PACX-1vRWBUuFWRsWIG4VPDY_GFqy0yK7TPzEogdw6a0jySJDtQGHdbJH7RNeZn6D_alAU2gvkSh5016DupBk/pub?w=519&h=688'
+                }
+                title='tutorial-thumbnail'
+              />
+              <Box position={'relative'}>
+                <h3>Banner Text</h3>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
