b523ec7ce210b4195a8afdbda977d72bde9a45e1
sendusyourbones <beckee.roos@gmail.com>
2023-07-21 18:41:42 -0700

    Add functionality to add single lesson from front-end

--- client/src/components/AddLesson.js
+++ client/src/components/AddLesson.js
@@ -1,12 +1,22 @@
+// React imports
 import { React, useState } from 'react';
+
+// Material UI imports
 import { 
+  Box,
   Button, 
   TextField, 
-  Typography 
 } from '@mui/material';
+
+// Imports for interacting with the db
+import { useMutation } from '@apollo/client';
+import { ADD_LESSON } from '../utils/mutations/lessonMutations';
+
+// Imports for other utilities
 import { isEmptyInput } from '../utils/validation';
 
 export function AddLesson() {
+  // Set default state values
   const inputDefaultValues = {
     value: '',
     isEmpty: false,
@@ -18,15 +28,27 @@ export function AddLesson() {
   const [media, setMedia] = useState(inputDefaultValues);
   const [duration, setDuration] = useState(inputDefaultValues);
 
-  function handleSubmit(e) {
+  // Set up mutation to add the tutorial to the db
+  const [addLesson, { error }] = useMutation(ADD_LESSON);
+
+  // When form is submitted, add the lesson to the db
+  async function handleSubmit(e) {
     e.preventDefault();
-    const data = new FormData(e.currentTarget);
-    console.log({
-      name: data.get('name'),
-      body: data.get('body'),
-      media: data.get('media'),
-      duration: data.get('duration'),
-    });
+
+    const variables = {
+      // Temporarily hard-coding tutorialId
+      tutorialId: '64bafa1562eda466115edd33',
+      name: name.value,
+      body: body.value,
+      media: media.value,
+      duration: parseInt(duration.value),
+    };
+
+    try {
+      await addLesson({ variables });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   // set a new value to the state.value associated to the text field that invokes this function
@@ -64,74 +86,72 @@ export function AddLesson() {
 
   return (
     <div>
-      <Typography component='h1' variant='h5'>
-        Add Lessons to Your Tutorial
-      </Typography>
-      <TextField
-        required
-        fullWidth
-        id='name'
-        name='name'
-        label='Name'
-        margin='normal'
-        onChange={(e) => handleOnChange(e.target.value.trim(), setName)}
-        onBlur={(e) => handleOnBlur(e.target.value, setName)}
-        error={name.isEmpty}
-        helperText={name.isEmpty && 'Please enter a name for your lesson'}
-        onFocus={() => handleOnFocus(name, setName)}
-      />
-      <TextField
-        required
-        fullWidth
-        id='body'
-        name='body'
-        label='Body'
-        margin='normal'
-        onChange={(e) => handleOnChange(e.target.value.trim(), setBody)}
-        onBlur={(e) => handleOnBlur(e.target.value, setBody)}
-        error={body.isEmpty}
-        helperText={
-          body.isEmpty && 'Please enter the body of your lesson'
-        }
-        onFocus={() => handleOnFocus(body, setBody)}
-      />
-      <TextField
-        fullWidth
-        id='media'
-        name='media'
-        label='Media'
-        margin='normal'
-        onChange={(e) => handleOnChange(e.target.value.trim(), setMedia)}
-      />
-      <TextField
-        required
-        fullWidth
-        id='duration'
-        name='duration'
-        label='Duration'
-        margin='normal'
-        onChange={(e) => handleOnChange(e.target.value.trim(), setDuration)}
-        onBlur={(e) => handleOnBlur(e.target.value, setDuration)}
-        error={duration.isEmpty}
-        helperText={
-          duration.isEmpty &&
-          'Please enter the duration of this lesson'
-        }
-        onFocus={() => handleOnFocus(duration, setDuration)}
-      />
-      <Button 
-        variant='contained' 
-        sx={{ mt: 3, mb: 2 }}
+      <Box
+        component='form'
+        noValidate
+        onSubmit={handleSubmit}
+        sx={{ mt: 1 }}
       >
-        Add Another Lesson
-      </Button>
-      <Button 
-        type='submit' 
-        variant='contained' 
-        sx={{ mt: 3, mb: 2 }}
-      >
-        Submit All Lessons
-      </Button>
+        <TextField
+          required
+          fullWidth
+          id='name'
+          name='name'
+          label='Name'
+          margin='normal'
+          onChange={(e) => handleOnChange(e.target.value.trim(), setName)}
+          onBlur={(e) => handleOnBlur(e.target.value, setName)}
+          error={name.isEmpty}
+          helperText={name.isEmpty && 'Please enter a name for your lesson'}
+          onFocus={() => handleOnFocus(name, setName)}
+        />
+        <TextField
+          required
+          fullWidth
+          id='body'
+          name='body'
+          label='Body'
+          margin='normal'
+          onChange={(e) => handleOnChange(e.target.value.trim(), setBody)}
+          onBlur={(e) => handleOnBlur(e.target.value, setBody)}
+          error={body.isEmpty}
+          helperText={
+            body.isEmpty && 'Please enter the body of your lesson'
+          }
+          onFocus={() => handleOnFocus(body, setBody)}
+        />
+        <TextField
+          fullWidth
+          id='media'
+          name='media'
+          label='Media'
+          margin='normal'
+          onChange={(e) => handleOnChange(e.target.value.trim(), setMedia)}
+        />
+        <TextField
+          required
+          fullWidth
+          id='duration'
+          name='duration'
+          label='Duration'
+          margin='normal'
+          onChange={(e) => handleOnChange(e.target.value.trim(), setDuration)}
+          onBlur={(e) => handleOnBlur(e.target.value, setDuration)}
+          error={duration.isEmpty}
+          helperText={
+            duration.isEmpty &&
+            'Please enter the duration of this lesson'
+          }
+          onFocus={() => handleOnFocus(duration, setDuration)}
+        />
+        <Button 
+          type='submit' 
+          variant='contained' 
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Save Lesson
+        </Button>
+      </Box>
     </div>
   );
 }
--- client/src/pages/AddLessons.js
+++ client/src/pages/AddLessons.js
@@ -1,9 +1,13 @@
 import { React } from 'react';
+import { Typography } from '@mui/material';
 import { AddLesson } from '../components';
 
 export function AddLessons() {
   return (
     <div>
+      <Typography component='h1' variant='h5'>
+        Add Lessons to Your Tutorial
+      </Typography>
       <AddLesson />
     </div>
   );
