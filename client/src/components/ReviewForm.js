951f8268d1124fa23c0971eee60326fae66219e0
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-07-23 14:07:32 -0700

    update database interaction code copied from AddLesson/AddTutorial

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -23,19 +23,21 @@ import { ADD_REVIEW } from '../utils/mutations/reviewMutations';
 import { isEmptyInput } from '../utils/validation';
 
 export function ReviewForm() {
-
-    // Set default state values
+  // Set default state values
   const inputDefaultValues = {
     value: '',
     isEmpty: false,
     isValid: true,
   };
-//Is there a way to get the username from the logged-in information? do we have a context for a logged-in user?
+  //Is there a way to get the username from the logged-in information? do we have a context for a logged-in user?
   const [username, setUsername] = useState(inputDefaultValues);
   const [comment, setComment] = useState(inputDefaultValues);
   const [loggedOut, setLoggedOut] = useState(false);
-  
-   // Set up mutation to add the lesson to the db
+
+  // Get tutorial ID from URL wildcard
+  const { tutorialId } = useParams();
+
+  // Set up mutation to add the lesson to the db
   // Use the cache to add each new lesson to the bottom of the page upon saving
   const [addReview, { error: reviewError }] = useMutation(ADD_REVIEW, {
     update(cache, { data: { addReview } }) {
@@ -72,18 +74,65 @@ export function ReviewForm() {
   // When form is submitted, add the tutorial to the db
   async function handleSubmit(e) {
     e.preventDefault();
-
-    const variables = {
-        comment: comment.value,
-        
-      };
-
     // If user is not logged in, set state to show error and exit submit function
     if (!user) {
       setLoggedOut(true);
       return;
     }
-  // Get tutorial ID from URL wildcard
-  const { tutorialId } = useParams();
 
-};
+    const variables = {
+      username: user._id,
+      comment: comment.value,
+    };
+    try {
+      await addReview({ variables });
+      // Reset all of the form fields after successful submission
+      resetFormFields(setName);
+      resetFormFields(setBody);
+      resetFormFields(setMedia);
+      resetFormFields(setDuration);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  // set a new value to the state.value associated to the text field that invokes this function
+  function handleOnChange(inputValue, setState) {
+    setState((otherValues) => ({
+      ...otherValues,
+      value: inputValue,
+    }));
+  }
+
+  // verify that the input is non-blank
+  // set the associated state to display the appropriate error message based on the validation result
+  function handleOnBlur(inputValue, setState) {
+    // ensure that the input is not empty
+    if (isEmptyInput(inputValue)) {
+      setState((otherValues) => ({
+        ...otherValues,
+        isEmpty: true,
+      }));
+      return;
+    }
+  }
+
+  // update the state to clear the error when the user focuses on that field
+  function handleOnFocus(state, setState) {
+    // change state to remove the error message on Focus if previously input was empty
+    if (state.isEmpty) {
+      setState((otherValues) => ({
+        ...otherValues,
+        isEmpty: false,
+      }));
+      return;
+    }
+  }
+
+  // Function to reset the form fields to their default values
+  function resetFormFields(setState) {
+    setState((otherValues) => ({
+      ...otherValues,
+      value: '',
+    }));
+  }
+}
