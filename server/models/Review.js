0e47fdb20eafd1a4e4eca904fbf962b6463f46cd
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-07-24 15:55:10 -0700

    updates to address review comments, ran Prettier on files associated with this PR

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,16 +1,16 @@
 const { Schema, model } = require('mongoose');
 
 const reviewSchema = new Schema({
-    reviewer: {
-          type: Schema.Types.ObjectId,
-          ref: 'User',
-        },
-    rating: {
-        type: Number,
-    },
-    comment: {
-        type: String,
-    },
+  reviewer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  rating: {
+    type: Number,
+  },
+  comment: {
+    type: String,
+  },
 });
 
 module.exports = model('Review', reviewSchema);
