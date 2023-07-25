cf928aad0011cb72d52c03f900152533a75b146c
sendusyourbones <beckee.roos@gmail.com>
2023-07-24 21:59:27 -0700

    Add donate link to footer and add text to page

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,41 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import { Typography } from '@material-ui/core';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from "../components/CheckoutForm";
+import CheckoutForm from '../components/CheckoutForm';
 
 export function Payment(props) {
-    const [stripePromise, setStripePromise] = useState(null);
-    const [clientSecret, setClientSecret] = useState("");
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState('');
 
-    useEffect(() => {
-        fetch('/config').then(async (res) => {
-            const { publishableKey } = await res.json();
-            
-            setStripePromise(loadStripe(publishableKey));
-        })
-    }, []);
+  useEffect(() => {
+    fetch('/config').then(async (res) => {
+      const { publishableKey } = await res.json();
 
-    useEffect(() => {
-        fetch('/create-payment-intent', {
-            method: 'POST',
-            body: JSON.stringify({}),
-        }).then(async (res) => {
-            const { clientSecret } = await res.json();
-            console.log(clientSecret)
-            setClientSecret(clientSecret);
-        })
-    }, []);
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
 
-    return (
-        <div>
-            <h1>Payment</h1>
-            {stripePromise && clientSecret && (
-                <Elements stripe={stripePromise} options={{ clientSecret }}>
-                    <CheckoutForm  />
-                </Elements>
-            )}
-        </div>
-    );
-};
+  useEffect(() => {
+    fetch('/create-payment-intent', {
+      method: 'POST',
+      body: JSON.stringify({}),
+    }).then(async (res) => {
+      const { clientSecret } = await res.json();
+      console.log(clientSecret);
+      setClientSecret(clientSecret);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Typography component='h1' variant='h4'>
+        Donate
+      </Typography>
+      <Typography component='p' style={{ marginBottom: '1rem' }}>
+        Love our site? Donate $10 below to show your appreciation. Thank you in
+        advance for the cups of coffee!
+      </Typography>
+      {stripePromise && clientSecret && (
+        <Elements stripe={stripePromise} options={{ clientSecret }}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
 
 export default Payment;
