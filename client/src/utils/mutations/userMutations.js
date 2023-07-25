340e19b231f91b329d157bfa5cad6809d220d5ed
UserOlena <helen.watermore@gmail.com>
2023-07-24 17:32:40 -0700

    Refactor User Mutation.

--- client/src/utils/mutations/userMutations.js
+++ client/src/utils/mutations/userMutations.js
@@ -1,8 +1,14 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation login(
+    $email: String!,
+     $password: String!
+  ) {
+    login(
+      email: $email, 
+      password: $password
+    ) {
       token
       user {
         _id
@@ -13,8 +19,16 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+  mutation addUser(
+    $username: String!, 
+    $email: String!, 
+    $password: String!
+  ) {
+    addUser(
+      username: $username, 
+      email: $email, 
+      password: $password
+    ) {
       token
       user {
         _id
@@ -25,8 +39,14 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_FAVORITE_TO_USER = gql`
-  mutation addFavoritetoUser($id: ID!, $tutorialId: ID!) {
-    addFavoritetoUser(_id: $id, tutorialId: $tutorialId) {
+  mutation addFavoritetoUser(
+    $id: ID!, 
+    $tutorialId: ID!
+  ) {
+    addFavoritetoUser(
+      _id: $id,
+      tutorialId: $tutorialId
+    ) {
       _id
     }
   }
