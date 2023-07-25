1a1aa12fe1cf396af747a9156937974395c48513
sendusyourbones <beckee.roos@gmail.com>
2023-07-25 16:57:17 -0700

    Restore old server code

--- server/server.js
+++ server/server.js
@@ -47,15 +47,8 @@ app.post('/create-payment-intent', async (req, res) => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
