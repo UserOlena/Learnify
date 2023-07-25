4529891282cf22c4e10321ef1bd1f7a4a6604c58
rslaughter12 <rslaughter12@gmail.com>
2023-07-24 21:23:15 -0700

    search bar and styling removed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -33,33 +33,6 @@ const useStyles = makeStyles((theme) => ({
     height: '80px',
     zoom: '1.2',
   },
-  search: {
-    display: 'flex',
-    alignItems: 'center',
-    margin: '0 auto',
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#888888',
-  },
-  inputRoot: {
-    width: '100%',
-  },
-  inputInput: {
-    padding: theme.spacing(1),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    color: '#333333',
-    fontSize: '16px',
-    '&::placeholder': {
-      color: '#888888',
-    },
-  },
   navButtons: {
     display: 'flex',
     justifyContent: 'flex-end',
@@ -74,23 +47,12 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: theme.spacing(1),
     },
   },
-  searchOpen: {
-    display: 'flex',
-    alignItems: 'center',
-    maxWidth: '1300px',
-    margin: '0 auto',
-    backgroundColor: 'inherit', // Set the background color to inherit the app bar color
-    '&:hover': {
-      backgroundColor: '#f5f5f5',
-    },
-  },
 }));
 
 export function Navbar() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const [categoriesMenuAnchorEl, setCategoriesMenuAnchorEl] = useState(null);
-  const [showSearchBar, setShowSearchBar] = useState(false);
 
   const { loading, data } = useQuery(GET_CATEGORIES);
   const categories = data?.categories || [];
@@ -113,30 +75,6 @@ export function Navbar() {
     setCategoriesMenuAnchorEl(null);
   }
 
-  function handleSearchClick() {
-    // Only show the search bar when it's not already open
-    if (!showSearchBar) {
-      setShowSearchBar(true);
-    }
-  }
-
-  function handleMenuItemKeyDown(event) {
-    // Prevent event propagation to parent elements when interacting with the search bar
-    event.stopPropagation();
-
-    if (event.key === 'Enter' && !showSearchBar) {
-      handleCloseMenus();
-    }
-  }
-
-  function handleSearchKeyDown(event) {
-    // Prevent event propagation to parent elements when interacting with the search bar
-    event.stopPropagation();
-  }
-
-  function handleSearchClose() {
-    setShowSearchBar(false);
-  }
 
   return (
     <AppBar position='static' className={classes.appBar}>
@@ -205,34 +143,6 @@ export function Navbar() {
             <MenuItem onClick={handleCloseMenus}>Settings</MenuItem>
           </NavLink>
         </>
-        )}
-        <MenuItem
-          onClick={handleSearchClick}
-          onKeyDown={handleMenuItemKeyDown}
-          tabIndex={showSearchBar ? -1 : 0}
-        >
-          <ListItemIcon>
-            <SearchIcon style={{ color: 'black' }} />
-          </ListItemIcon>
-          <ListItemText primary='Search' />
-        </MenuItem>
-        {showSearchBar && (
-          <MenuItem onClick={handleSearchClick}>
-            <div className={classes.searchOpen}>
-              <div className={classes.searchIcon}>
-                <SearchIcon style={{ color: '#888888' }} />
-              </div>
-              <InputBase
-                placeholder='Search...'
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                onBlur={handleSearchClose}
-                onKeyDown={handleSearchKeyDown}
-              />
-            </div>
-          </MenuItem>
         )}
       </Menu>
     </AppBar>
